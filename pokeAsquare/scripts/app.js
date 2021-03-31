f738964e975b2084859b1d75f2ec219dffb5f9bb
Parvis H <phaydark@gmail.com>
2021-03-31 01:31:52 -0600

    initial commit

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -6,3 +6,16 @@ console.log('Welcome to Poke-A-Square...');
 // When the user clicks on a color: the color should disapear and score should be added or subtracted.
 
 // When the round is over, the scores round and timer should be updated for the user to start over with increased difficulty.
+
+const createSquares = numberOfSquares => {
+    const $squares = $(".squares");
+    for (let i = 1; i <= numberOfSquares; i++) {
+        const $square = $('<div class= "square" />');
+        $square.css("background-color", applyRandomColor());
+        $squares.append($square);
+    }
+}
+const applyRandomColor = () => {
+
+}
+createSquares(25);
